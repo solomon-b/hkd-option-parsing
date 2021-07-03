2fc635f84ad67c1963b829347c3c3929965ce6db
Solomon Bothwell <ssbothwell@gmail.com>
2021-07-02 19:46:25 -0700

    Adds big warnining about Last semigroup

--- src/Options.hs
+++ src/Options.hs
@@ -28,6 +28,38 @@ newtype Last a = Last { getLast :: Maybe a }
                  , Monad
                  )
 
+-- This solution is so horrible.  When using the `Partial` alias from
+-- Higgledy, we wrap our data in `Last`. This represents data capture
+-- that can fail.
+--
+-- In this demo we use `Partial Options` to construct incomplete
+-- `Options` for each data source and then `<>` them together to
+-- produce the complete `Option` value.
+--
+-- We then use `construct` to convert from `Partial Options` to `Maybe
+-- Options`. This is really nice because it gets rid of all the
+-- unnecessary Maybes.
+
+-- However, this causes problems if your Options type contains a field
+-- that is truly optional (Maybe MyField) AND it can be supplied by
+-- multiple data sources. In this scenario the optional `MyField`
+-- looks like this while in `Partial`:
+--
+-- Last (Maybe MyField)
+--
+-- So if we are merging two of these values together and the second
+-- one is `Last (Just Nothing)` then `<>` will choose that value over
+-- a `Last (Just (Just x))`:
+--
+-- > Last (Just $ Just True) <> Last (Just Nothing)
+-- Last {getLast = Just Nothing}
+--
+-- The behavior we want is this:
+-- > Last (Just $ Just True) <> Last (Just Nothing)
+-- Last {getLast = Just (Just True)}
+--
+-- This Semigroup instance accomplishes that but looks like a potential
+-- major footgun.
 instance {-# OVERLAPPING #-} Semigroup (Last (Maybe a)) where
  (<>) (Last Nothing) b = b
  (<>) a (Last Nothing) = a
@@ -54,7 +86,7 @@ data Options = Options
   , metadataDB :: URI
 } deriving (Show, Generic)
 
-type Partial a = HKD a  Last
+type Partial a = HKD a Last
 
 emptyOptions :: Partial Options
 emptyOptions = mempty
